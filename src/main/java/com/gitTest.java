a3fdfe4d1623228bb2ad568353cd765444dc226c
yang <447703411@qq.com>
2021-02-01 18:53:42 +0800

    first commit dev commit master commit

--- src/main/java/com/gitTest.java
+++ src/main/java/com/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello world...");
         System.out.println("dev...");
+        System.out.println("master....");
     }
 }
